8ace8af790032a29c4187eb7e53125326664d595
Lucas Löfstedt <Lucas.bronegardlofstedt@elev.ga.ntig.se>
2023-09-21 10:57:07 +0200

    slumptal

---
+++ fightSIM/Program.cs
@@ -0,0 +1,23 @@
+﻿// SLUMP
+Random generator = new Random();
+
+int slumptal = generator.Next(101);
+
+Console.WriteLine(slumptal);
+
+
+// WHILE-LOOP
+
+
+int tal = 100;
+
+while (tal > 0)
+{
+    Console.WriteLine(tal);
+    tal--;
+}
+
+Console.WriteLine("FÄRDIG");
+
+
+Console.ReadLine();
